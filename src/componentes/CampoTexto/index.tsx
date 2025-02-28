10e36bd93a57739fd9b25ba82d9c0c89186a14de
Mylena Marques Ribeiro <ribeiro.mylena1@gmail.com>
2025-02-28 13:55:48 -0300

    Criando um novo campo - Data

--- src/componentes/CampoTexto/index.tsx
+++ src/componentes/CampoTexto/index.tsx
@@ -6,14 +6,14 @@ interface CampoTextoProps {
     label: string;
     valor: string;
     obrigatorio?: boolean;
+    tipo?: 'text' | 'password' | 'date' | 'email' | 'number';
 }
 
-const CampoTexto = ({ aoAlterado, label, placeholder, valor, obrigatorio = false }: CampoTextoProps) => {
+const CampoTexto = ({ aoAlterado, label, placeholder, valor, obrigatorio = false, tipo = 'text' }: CampoTextoProps) => {
 
     const placeholderModificada = `${placeholder}...` 
 
     const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
-        // props.aoAlterado(evento.target.value)
         aoAlterado(evento.target.value);
     }
 
@@ -27,6 +27,7 @@ const CampoTexto = ({ aoAlterado, label, placeholder, valor, obrigatorio = false
                 onChange={aoDigitado} 
                 required={obrigatorio} 
                 placeholder={placeholderModificada}
+                type={tipo}
             />
         </div>
     )
--- src/componentes/Time/index.tsx
+++ src/componentes/Time/index.tsx
@@ -23,11 +23,11 @@ const Time = (props: TimeProps) => {
                     nome={colaborador.nome} 
                     cargo={colaborador.cargo} 
                     imagem={colaborador.imagem}
+                    data={colaborador.data}
                 /> )}
             </div>
         </section> 
         : <React.Fragment></React.Fragment>
-        // : ''
     )
 }
 
